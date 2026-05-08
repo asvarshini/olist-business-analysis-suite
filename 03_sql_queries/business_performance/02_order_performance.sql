8d081890cb36deb600e7cdac49d92f6a75af9f74
VarshiniAS <asvarshini84@gmail.com>
2026-05-08 10:18:25 +0530

    Completed order performannce analysis module

---
+++ 03_sql_queries/business_performance/02_order_performance.sql
@@ -0,0 +1,72 @@
+-- =====================================================
+-- BUSINESS PERFORMANCE ANALYSIS
+-- Order Performance Analysis
+-- =====================================================
+
+-- =====================================================
+-- KPI 1 : TOTAL ORDERS
+-- =====================================================
+
+SELECT
+COUNT(DISTINCT order_id) AS total_orders
+FROM orders;
+
+-- =====================================================
+-- KPI 2 : TOTAL PRODUCT ORDERS
+-- =====================================================
+
+SELECT
+COUNT(order_item_id) AS total_product_orders
+FROM order_items;
+
+-- =====================================================
+-- KPI 3 : MONTHLY ORDER TREND
+-- =====================================================
+
+SELECT
+DATE_FORMAT(order_purchase_timestamp, '%Y-%m') AS monthly_purchase,
+COUNT(DISTINCT order_id) AS total_orders
+FROM orders
+GROUP BY monthly_purchase
+ORDER BY monthly_purchase;
+
+-- =====================================================
+-- KPI 4 : AVERAGE ORDERS PER CUSTOMER
+-- =====================================================
+
+SELECT
+AVG(t.total_orders) AS avg_orders_per_customer
+FROM (
+SELECT
+c.customer_unique_id,
+COUNT(o.order_id) AS total_orders
+FROM customers c
+JOIN orders o
+ON c.customer_id = o.customer_id
+GROUP BY c.customer_unique_id
+) t;
+
+-- =====================================================
+-- KPI 5 : ORDER STATUS DISTRIBUTION
+-- =====================================================
+
+SELECT
+order_status,
+COUNT(*) AS total_orders
+FROM orders
+GROUP BY order_status
+ORDER BY total_orders DESC;
+
+-- =====================================================
+-- KPI 6 : AVERAGE ORDER VALUE (AOV)
+-- =====================================================
+
+SELECT
+AVG(t.order_total) AS average_order_value
+FROM (
+SELECT
+order_id,
+SUM(price) AS order_total
+FROM order_items
+GROUP BY order_id
+) t;
